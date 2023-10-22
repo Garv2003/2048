68a24f142bc9441decde345266cfa6bc39e69da6
Garv05 <coolgarv9@gmail.com>
2023-10-23 01:29:38 +0530

    changes

--- Grid.js
+++ Grid.js
@@ -1,15 +1,22 @@
 const GRID_SIZE = 4;
 const CELL_SIZE = 14;
 const CELL_GAP = 2;
-
+var score = 0;
+const scoreElement = document.querySelector(".scoreno");
+const bestscoreElement = document.querySelector(".bestno");
+if (localStorage.getItem("bestscore") == null) {
+  localStorage.setItem("bestscore", 0);
+} else {
+  bestscoreElement.textContent = localStorage.getItem("bestscore");
+}
 export default class Grid {
   #cells;
+
   constructor(gridElement) {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
-
-    this.cells = createCellElements(gridElement).map((cellElement, index) => {
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
       return new Cell(
         cellElement,
         index % GRID_SIZE,
@@ -18,13 +25,33 @@ export default class Grid {
     });
   }
 
+  get cells() {
+    return this.#cells;
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || [];
+      cellGrid[cell.y][cell.x] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || [];
+      cellGrid[cell.x][cell.y] = cell;
+      return cellGrid;
+    }, []);
+  }
+
   get #emptyCells() {
-    return this.cells.filter((cell) => cell.title == null);
+    return this.#cells.filter((cell) => cell.tile == null);
   }
 
   randomEmptyCell() {
-    const randomeIndex = Math.floor(Math.random() * this.#emptyCells.length);
-    return this.#emptyCells[randomeIndex];
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+    return this.#emptyCells[randomIndex];
   }
 }
 
@@ -33,6 +60,8 @@ class Cell {
   #x;
   #y;
   #tile;
+  #mergeTile;
+
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement;
     this.#x = x;
@@ -40,11 +69,11 @@ class Cell {
   }
 
   get x() {
-    return this.#x
+    return this.#x;
   }
 
   get y() {
-    return this.#y
+    return this.#y;
   }
 
   get tile() {
@@ -53,11 +82,40 @@ class Cell {
 
   set tile(value) {
     this.#tile = value;
-    if (value == null) {
-      return;
-    }
+    if (value == null) return;
     this.#tile.x = this.#x;
     this.#tile.y = this.#y;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value === tile.value)
+    );
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return;
+    this.tile.value = this.tile.value + this.mergeTile.value;
+    score += this.tile.value;
+    if (score > bestscoreElement.textContent) {
+      bestscoreElement.textContent = score;
+      localStorage.setItem("bestscore", score);
+    }
+    scoreElement.textContent = score;
+    this.mergeTile.remove();
+    this.mergeTile = null;
   }
 }
 
